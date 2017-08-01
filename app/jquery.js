7ef43a4e7aafb2261d1196f85f0f8bb9da83a721
Miho Tse <mihotse@gmail.com>
2017-08-01 16:24:13 +0100

    Edit jquery.js

--- app/jquery.js
+++ app/jquery.js
@@ -1,16 +1,25 @@
-(function(exports) {
-	var inputListener = function() {
+var inputListener;
+
+	$(document).ready(function(){
+		inputListener = function() {
 		$('#textbox').on("keydown", function(e) {
 			if (e.which == '13') {
 				userInput = $('#textbox').val();
 				if (userInput != '') {
 					// Here we are taking user input and bot response.
-					// We then covert into html elements.
+					// We then convert into html elements.
+					log = document.getElementById('log');
+					console.log(log);
+			    userPara = document.createElement('p');
+			    botPara = document.createElement('p');
+			    userPara.innerHTML = userInput;
+			    botPara.innerHTML = "Have you tried drinking more?"
+			    log.appendChild(userPara);
+			    log.appendChild(botPara);
 				} else {
 					console.log("empty field")
 				}
 			};
 		});
-	}
-	exports.inputListener = inputListener;
-})(this);
+	};
+});
