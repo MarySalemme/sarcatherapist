365f6ed538e3d4d1a0db8237ed31d7c58b58ddf8
MarySalemme <marysalemme1@gmail.com>
2017-08-01 12:21:03 +0100

    Editing if/else statement

--- app/models/bot.js
+++ app/models/bot.js
@@ -5,9 +5,11 @@ function Bot() {
 Bot.prototype.produceResponse = function(emotion) {
   if (emotion.toLowerCase() === "sad") {
     this._response = "Have you tried drinking?";
-  } else if (emotion.toLowerCase() === "neutral") {
+  } else if (emotion.toLowerCase() === "ok") {
     this._response = "Can you be less boring, please?";
-  } else {
+  } else if (emotion.toLowerCase() === "happy") {
     this._response = "It's a trap";
-  };
+  } else {
+    this._response = "Wow dude. You are dead inside, just like me."
+  }
 };
--- spec/unit/botSpec.js
+++ spec/unit/botSpec.js
@@ -7,18 +7,23 @@ describe("Bot", function() {
 
   describe("produceResponse", function() {
     it("gives a response to a sad user", function () {
-      bot.produceResponse("sad")
+      bot.produceResponse("sad");
       expect(bot._response).toEqual("Have you tried drinking?");
     });
 
-    it("gives a response to a neutral user", function(){
-      bot.produceResponse("neutral")
+    it("gives a response to an ok user", function(){
+      bot.produceResponse("ok");
       expect(bot._response).toEqual("Can you be less boring, please?");
     });
 
     it("gives a response to a happy user", function(){
-      bot.produceResponse("happy")
+      bot.produceResponse("happy");
       expect(bot._response).toEqual("It's a trap");
+    });
+
+    it("gives a response to a user who is dead inside", function(){
+      bot.produceResponse("dead");
+      expect(bot._response).toEqual("Wow dude. You are dead inside, just like me.");
     });
   });
 });
