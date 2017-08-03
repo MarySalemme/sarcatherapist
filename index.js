35ff6168fc01f6b501e05f65a91aa6fa250730d9
MarySalemme <marysalemme1@gmail.com>
2017-08-03 17:15:55 +0100

    Creating server.js file

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.end();
-});
-
-server.listen(8080);
-
 window.onload = function() {
   var controller = new Controller();
   controller.inputListener($, document);
