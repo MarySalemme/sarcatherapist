52a96fdfe4ebc45291a09d18adf6999edd067859
Miho Tse <mihotse@gmail.com>
2017-08-03 15:23:32 +0100

    Edit index.js with port listening

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ window.onload = function() {
   var controller = new Controller();
   controller.inputListener($, document);
 };
+
+var port = process.env.PORT || 8000
+
+let server = require('http').Server(app);
+
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
