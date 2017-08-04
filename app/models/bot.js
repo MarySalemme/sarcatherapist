8b5ca831116f4945b4bbb79ad8c3d02faece2a7a
Jasper King-Harman <j.kingharman@googlemail.com>
2017-08-04 15:31:25 +0100

    Extending checks and connecting extracted dictionary

--- app/models/bot.js
+++ app/models/bot.js
@@ -1,6 +1,9 @@
 function Bot(parser) {
   this._inputType = null,
-  this._greetingKeyWords = greetKey
+  this._greetingKeyWords = greetKey,
+  this._seriousKeyWords = seriousKey,
+  this._mildNegativeKeyWords = mildNegativeKey,
+  this._yesKeyWords = yesKey,
   this._curseKeyWords = ["substitute", "substitute", "substitute"],
   this._parser = parser
 }
@@ -19,19 +22,45 @@ Bot.prototype._checkForGreeting = function(userInput) {
   (input) })
 };
 
+Bot.prototype._checkForYes = function(userInput) {
+  var self = this
+  return userInput.some(function(input) { return self._yesKeyWords.includes
+  (input) })
+};
+
+Bot.prototype._checkForMildNegative = function(userInput) {
+  var self = this
+  return userInput.some(function(input) { return self._mildNegativeKeyWords.includes
+  (input) })
+};
+
+
 Bot.prototype._checkForCurse = function(userInput) {
   var self = this
   return userInput.some(function(input) { return self._curseKeyWords.includes
   (input) })
 };
 
+Bot.prototype._checkForSerious = function(userInput) {
+  var self = this
+  return userInput.some(function(input) { return self._seriousKeyWords.includes
+  (input) })
+};
+
+
 Bot.prototype._categoriseUserInput = function(userInput) {
   if (this._checkForGreeting(userInput)) {
-    this._inputType = "greeting"
+      this._inputType = "greeting"
   } else if (this._checkForCurse(userInput)) {
-    this._inputType = "curse"
+      this._inputType = "curse"
+  }  else if (this._checkForSerious(userInput)) {
+      this._inputType = "serious"
+  }  else if (this._checkForMildNegative(userInput)) {
+      this._inputType = "mild"
+  }  else if (this._checkForYes(userInput)) {
+      this._inputType = "yes"
   } else {
-    this._inputType = "generic"
+      this._inputType = "generic"
   }
 };
 
@@ -44,17 +73,31 @@ Bot.prototype._sampledCurseResponse = function () {
     return curseResponse;
 };
 
+Bot.prototype._sampledMildResponse = function () {
+  var mildResponse = mildNegativeResponse[Math.floor(Math.random() * mildNegativeResponse.length)];
+  return mildResponse
+};
+
+Bot.prototype._sampledSeriousResponse = function () {
+  return seriousResponse
+};
+
+Bot.prototype._sampledYesResponse = function () {
+  var yeahResponse = yesResponse[Math.floor(Math.random() * yesResponse.length)];
+  return yeahResponse
+};
+
 Bot.prototype._sampledGreetingResponse = function () {
   var greetingResponse = greetResponse[Math.floor(Math.random() * greetResponse.length)];
   return greetingResponse
 };
 
 Bot.prototype._sampledGenericResponse = function () {
-  var genericResponse = "Have you tried drinking more?"
-  return genericResponse
+  var genResponse = genericResponse[Math.floor(Math.random() * genericResponse.length)];
+  return genResponse
 };
 
 Bot.prototype._pickAppropriateResponse = function () {
-  hash = {"curse": this._sampledCurseResponse(), "greeting": this._sampledGreetingResponse(), "generic": this._sampledGenericResponse()}
+  hash = {"curse": this._sampledCurseResponse(), "yes": this._sampledYesResponse(), "mild": this._sampledMildResponse(), "serious": this._sampledSeriousResponse(), "greeting": this._sampledGreetingResponse(), "generic": this._sampledGenericResponse()}
   return hash[this._inputType]
 }
--- app/models/dir.js
+++ app/models/dir.js
@@ -17,6 +17,14 @@ var greetKey = [
   "elloello"
 ]
 
+var greetResponse = [
+  "Oh, it's you let's make this quick",
+  "Come into my cyber-office",
+  "Not you again",
+  "I have issues. You have issues. Let's chat"
+]
+
+
 var seriousKey = [
  "suicide",
  "suicidal",
@@ -31,18 +39,48 @@ var seriousKey = [
 ]
 
 var seriousResponse = [
-  
+  "Time to refill your prozac"
+]
+
+var genericResponse = [
+  "Have you tried drinking more?",
+  "Have you tried getting over yourself?",
+  "What am I, psychic? Tell me how you *feel*",
+  "How are you feeling - I don't have all day"
+]
+
+var yesKey = [
+  "yes",
+  "yep",
+  "yeah",
+  "okay",
+  "sure",
+  "certainly",
+  "absolutely",
+  "alright"
+]
+
+var yesResponse = [
+  "Well, time's up then",
+  "Sure, sure"
 ]
 
 
-var greetResponse = [
-  "Oh, it's you let's make this quick",
-  "Come into my cyber-office",
-  "Not you again",
-  "I have issues. You have issues. Let's chat"
+
+var mildNegativeKey = [
+  "tired",
+  "bored",
+  "distracted",
+  "stressed",
+  "weary",
+  "exhausted",
+  "low",
+  "down",
+  "blue"
 ]
 
-
-// var genericResponse = [
-//   "Therapy is for the weak"
-// ]
+var mildNegativeResponse = [
+  "There is no cure for your disease",
+  "Take a break: go to the pub",
+  "Maybe you're just a depressive",
+]
